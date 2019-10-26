2c9149ba60fba354e1fad3450ba203f346f26487
victor <vi350@inbox.ru>
2019-10-27 02:23:56 +0300

    main.go cleaned

--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-<<<<<<< HEAD
-	"crypto/rand"
-	"encoding/base64"
-	"github.com/gin-contrib/cors"
-=======
->>>>>>> 3cce0e48ae888be3083202ef3842a30631ebf0f3
+	_ "github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/vi350/spbstu-hackathon-autumn19/Auth"
 	"github.com/vi350/spbstu-hackathon-autumn19/Basics"
@@ -40,37 +35,13 @@ func main() {
 	DB.ConnectDB()
 	DB.CreateTables()
 
-<<<<<<< HEAD
-	//var user Model.User
-	//var token string
-	//token, _ = GenerateRandomString(15)
-	//_, _ = DB.DB.Model(&user).Set("token = ?", token).Where("id = ?", "321").Returning("*").Update()
-	//
-	//err = app.Run(":8080")
-	//if err != nil {
-	//	log.Panic(err)
-	//}
-}
-
-func GenerateRandomBytes(n int) ([]byte, error) {
-	b := make([]byte, n)
-	_, err := rand.Read(b)
-	// Note that err == nil only if we read len(b) bytes.
-=======
 	DB.SelectBySkills([]string{"go","gin"},"pdrs")
 
 
 	//log.Fatal(autotls.Run(app, "example1.com", "example2.com"))
 
 	err = app.Run(":8080")
->>>>>>> 3cce0e48ae888be3083202ef3842a30631ebf0f3
 	if err != nil {
-		return nil, err
+		log.Panic(err)
 	}
-	return b, nil
-}
-
-func GenerateRandomString(s int) (string, error) {
-	b, err := GenerateRandomBytes(s)
-	return base64.URLEncoding.EncodeToString(b), err
 }
